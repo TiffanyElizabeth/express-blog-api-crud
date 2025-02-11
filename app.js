b68fc365a58eb7fcbe68e29902b2fdc702b5bb11
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-02-11 15:49:50 +0100

    feat: add 05/02 ex

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 const express = require("express"); // import express
 const app = express(); // use function express to start the appliation (web server)
+const cors = require("cors");
 const port = 3000; // port used to run the application 
+const postsRouter = require("./router/postsRouter"); // router
 const errorsHandler = require("./middleware/errorsHandler"); //import errors handler middleware 
 const notFound = require("./middleware/notFound"); //import not found middleware
 
+// http://localhost:3000/
 
-// app = webserver, .get = il metodo http su cui voglio restituire la risposta
-// il primo parametre "/" = la rotta (url) sui cui rimango in ascoloto di eventuali richieste 
-// il secondo parametre e una funzione (req, res) => {} - la funzione che gestirà la richiesta da parte del cliente e restiuirà la risposta 
+
 app.get('/', (req, res) => {
   res.send('server del mio blog') // res.send invia un contenuto testuale/html (slide 23 express intro)
 });
 
-const postsRouter = require("./router/postsRouter"); // router
-
 app.use(express.static("public")); // middleware per file statici
 
 app.use(express.json()); // middleware to tranform req body i)
+
+app.use(cors({ origin: "http://localhost:5174/" }));
 
 app.use("/posts", postsRouter);
 
